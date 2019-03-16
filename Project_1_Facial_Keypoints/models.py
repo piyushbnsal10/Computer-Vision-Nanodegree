529288d684745392c18d05854a7159bb5ca9a2dd
piyush <piyush.bnsal10@gmail.com>
2019-03-16 18:17:47 +0530

    Keypts Project

--- Project_1_Facial_Keypoints/models.py
+++ Project_1_Facial_Keypoints/models.py
@@ -20,18 +20,85 @@ class Net(nn.Module):
         
         # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
         # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
-        self.conv1 = nn.Conv2d(1, 32, 5)
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=96, kernel_size=(4, 4), stride=4, padding=0) # VALID
+        self.conv2 = nn.Conv2d(in_channels=96, out_channels=256, kernel_size=(5, 5), stride=1, padding=2) # SAME
+        self.conv3 = nn.Conv2d(in_channels=256, out_channels=384, kernel_size=(3, 3), stride=1, padding=1) # SAME
+        self.conv4 = nn.Conv2d(in_channels=384, out_channels=384, kernel_size=(3, 3), stride=1, padding=1) # SAME
+        self.conv5 = nn.Conv2d(in_channels=384, out_channels=256, kernel_size=(3, 3), stride=1, padding=1) # SAME
         
-        ## Note that among the layers to add, consider including:
-        # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
+        ## Max-Pool layer 
+        self.pool = nn.MaxPool2d(kernel_size=3, stride=2)
         
+        ## Linear layers
+        self.fc1 = nn.Linear(in_features=9216, out_features=4096)
+        self.fc2 = nn.Linear(in_features=4096, out_features=4096)
+        self.fc3 = nn.Linear(in_features=4096, out_features=136)
+        
+        ## Dropout 
+        self.dropout2 = nn.Dropout(p=0.2)
+        self.dropout4 = nn.Dropout(p=0.4)
+        self.dropout6 = nn.Dropout(p=0.6)
+        
+        # Batch Normalization
+        self.bn1 = nn.BatchNorm2d(num_features=96, eps=1e-05)
+        self.bn2 = nn.BatchNorm2d(num_features=256, eps=1e-05)
+        self.bn3 = nn.BatchNorm2d(num_features=384, eps=1e-05)
+        self.bn4 = nn.BatchNorm2d(num_features=384, eps=1e-05)
+        self.bn5 = nn.BatchNorm2d(num_features=256, eps=1e-05)
+        self.bn6 = nn.BatchNorm1d(num_features=4096, eps=1e-05)
+        self.bn7 = nn.BatchNorm1d(num_features=4096, eps=1e-05)
+        
+        ## Local response normalization
+        # if size=r=2 and a neuron has a strong activation, it will inhibit the activation
+        # of the neurons located in the feature maps immediately above and below its own.
+#         self.lrn = LocalResponseNorm(size=2, alpha=0.00002, beta=0.75, k=1)  # lrn is on new pytorch version apparently
+        
+        # Custom weights initialization
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                m.weight = nn.init.xavier_uniform(m.weight, gain=1)
+            elif isinstance(m, nn.Linear):
+                # FC layers have weights initialized with Glorot uniform initialization
+                m.weight = nn.init.xavier_uniform(m.weight, gain=1)
 
-        
     def forward(self, x):
-        ## TODO: Define the feedforward behavior of this model
-        ## x is the input image and, as an example, here you may choose to include a pool/conv step:
-        ## x = self.pool(F.relu(self.conv1(x)))
         
+        ## Conv layers
+        x = F.elu(self.conv1(x))
+        x = self.pool(x)
+        x = self.dropout2(x)
         
-        # a modified x, having gone through all the layers of your model, should be returned
+        x = F.elu(self.conv2(x))
+        x = self.bn2(x)
+        x = self.pool(x)
+#         x = self.dropout2(x)
+        
+        x = F.elu(self.conv3(x))
+        x = self.bn3(x)
+        x = self.dropout4(x)
+        
+        x = F.elu(self.conv4(x))
+        x = self.bn4(x)
+        x = self.dropout4(x)
+        
+        x = F.elu(self.conv5(x))
+        x = self.bn5(x)
+        x = self.pool(x)
+#         x = self.dropout4(x)
+
+        ## Flatten
+        x = x.view(x.size(0), -1) 
+        
+        ## Fully connected layers
+        x = F.elu(self.fc1(x))
+        x = self.bn6(x)
+        x = self.dropout6(x)
+        
+        x = F.elu(self.fc2(x))
+        x = self.bn6(x)
+        x = self.dropout6(x)
+        
+#         x = F.tanh(self.fc3(x))
+        x = self.fc3(x)
+    
         return x
